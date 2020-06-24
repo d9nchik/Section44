9a304bb805b0a0b6e4208b4bd35938972e8082c5
d9nich <dan.galaiko@gmail.com>
2020-06-24 22:39:16 +0300

    StringTest improvement.

--- src/com/d9nich/StringTest.java
+++ src/com/d9nich/StringTest.java
@@ -6,41 +6,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class StringTest {
-    private String string = "Hello, World! Big query in your web site";
+    private final String bigString = "Hello, World! Big query in your web site";
 
     @Test
     public void lengthTest() {
-        string = "";
-        assertEquals(0, string.length());
-
-        string = "Hello, world";
-        assertEquals(12, string.length());
-
-        string = "Super-puper long sentence";
-        assertEquals(25, string.length());
+        String emptyString = "";
+        assertEquals(0, emptyString.length());
+        String mediumString = "Hello, world";
+        assertEquals(12, mediumString.length());
     }
 
-    @SuppressWarnings("ResultOfMethodCallIgnored")
     @Test
     public void charAtTest() {
-        assertEquals('H', string.charAt(0));
-        assertEquals('e', string.charAt(39));
-        assertEquals('q', string.charAt(18));
-        assertThrows(IndexOutOfBoundsException.class, () -> string.charAt(40));
+        assertEquals('H', bigString.charAt(0));
+        assertEquals('q', bigString.charAt(18));
     }
 
     @Test
     public void substringTest() {
-        assertEquals("Hello", string.substring(0, 5));
-        assertEquals("site", string.substring(36));
+        assertEquals("Hello", bigString.substring(0, 5));
+        assertEquals("site", bigString.substring(36));
     }
 
     @Test
     public void indexOfTest() {
-        assertEquals(14, string.indexOf('B'));
-        assertEquals(36, string.indexOf("site"));
-        assertEquals(15, string.indexOf('i', 10));
-        assertEquals(13, string.indexOf(" ", 10));
-        assertEquals(-1, string.indexOf("aux", 10));
+        assertEquals(14, bigString.indexOf('B'));
+        assertEquals(36, bigString.indexOf("site"));
+        assertEquals(15, bigString.indexOf('i', 10));
+        assertEquals(13, bigString.indexOf(" ", 10));
+        assertEquals(-1, bigString.indexOf("aux", 10));
     }
 }
