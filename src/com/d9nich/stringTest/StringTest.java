800492d835f49d50961327b848486279b7df1746
d9nich <dan.galaiko@gmail.com>
2020-06-24 13:36:32 +0300

    Exercise1 has been done. I have written some test for String class. In StringTest, we have tested functionality of functions length, charAt, substring and indexOf.

---
+++ src/com/d9nich/stringTest/StringTest.java
@@ -0,0 +1,46 @@
+package com.d9nich.stringTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class StringTest {
+    private String string = "Hello, World! Big query in your web site";
+
+    @Test
+    public void lengthTest() {
+        string = "";
+        assertEquals(0, string.length());
+
+        string = "Hello, world";
+        assertEquals(12, string.length());
+
+        string = "Super-puper long sentence";
+        assertEquals(25, string.length());
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @Test
+    public void charAtTest() {
+        assertEquals('H', string.charAt(0));
+        assertEquals('e', string.charAt(39));
+        assertEquals('q', string.charAt(18));
+        assertThrows(IndexOutOfBoundsException.class, () -> string.charAt(40));
+    }
+
+    @Test
+    public void substringTest() {
+        assertEquals("Hello", string.substring(0, 5));
+        assertEquals("site", string.substring(36));
+    }
+
+    @Test
+    public void indexOfTest() {
+        assertEquals(14, string.indexOf('B'));
+        assertEquals(36, string.indexOf("site"));
+        assertEquals(15, string.indexOf('i', 10));
+        assertEquals(13, string.indexOf(" ", 10));
+        assertEquals(-1, string.indexOf("aux", 10));
+    }
+}
